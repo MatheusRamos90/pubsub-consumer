bcc9a7fc98738a0eb36d91350bdce2a6bbb7ba89
Matheus Ramos <matheus.hrs@live.com>
2023-11-17 12:21:01 -0300

    first commit

---
+++ src/main/java/br/com/matheushramos/pubsubconsumer/PubsubConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheushramos.pubsubconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubConsumerApplication.class, args);
+	}
+
+}
